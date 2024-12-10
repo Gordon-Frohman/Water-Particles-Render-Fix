3f223ce1a080806757ef2a047892c68bc273d5a0
Gordon-Frohman <sanisimov6699@gmail.com>
2024-12-10 19:30:39 +0700

    Made import more specific

--- src/main/java/su/sergiusonesimus/prf/mixin/mixins/minecraft/client/particle/MixinEntityFireworkStarterFX.java
+++ src/main/java/su/sergiusonesimus/prf/mixin/mixins/minecraft/client/particle/MixinEntityFireworkStarterFX.java
@@ -1,6 +1,6 @@
 package su.sergiusonesimus.prf.mixin.mixins.minecraft.client.particle;
 
-import net.minecraft.client.particle.*;
+import net.minecraft.client.particle.EntityFireworkStarterFX;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
--- src/main/java/su/sergiusonesimus/prf/mixin/mixins/minecraft/client/particle/MixinEntityFishWakeFX.java
+++ src/main/java/su/sergiusonesimus/prf/mixin/mixins/minecraft/client/particle/MixinEntityFishWakeFX.java
@@ -1,6 +1,6 @@
 package su.sergiusonesimus.prf.mixin.mixins.minecraft.client.particle;
 
-import net.minecraft.client.particle.*;
+import net.minecraft.client.particle.EntityFishWakeFX;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
